4e6877aa6b33f81f42d3d6ae3a04eeac97bfaa41
Yaroslaw Homziak <ywgk0102@gmail.com>
2023-07-07 19:07:14 +0300

    update content and scripts

--- main.js
+++ main.js
@@ -3,12 +3,37 @@ let myVideo = null;
 let myVideoEntity = null;
 
 let isFinde = false;
+let div = document.createElement('div');
 
-const content = document.querySelector('.content');
-content.style.display = "none";
+window.onload = () => {
+  const content = document.querySelector('#baseContant');
+  div.style.display = "none";
+  content.style.display = "block";
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-  content.style.display = "block";
+
+  const content = document.querySelector('#baseContant');
+  console.log(content);
+  content.style.display = "none";
+  // Встановлення тексту "Loading"
+  div.innerHTML = 'Loading...';
+  
+  // Стилізація елементу
+  div.style.position = 'fixed';
+  div.style.fontSize = "25px"
+  div.style.top = '50%';
+  div.style.left = '50%';
+  div.style.transform = 'translate(-50%, -50%)';
+  div.style.backgroundColor = 'white';
+  div.style.padding = '20px';
+  div.style.borderRadius = '5px';
+  //div.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+  div.style.zIndex = '9999';
+  
+  // Додавання елементу до body
+  document.body.appendChild(div);
+  
 	const sceneEl = document.querySelector('a-scene');
 	let arSystem;
 	sceneEl.addEventListener('loaded', function () {
@@ -27,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
       myVideo.pause();
 	});
 	sceneEl.addEventListener("arReady", (event) => {
+    console.log("loaded!");
 	});
 	sceneEl.addEventListener("arError", (event) => {
 	});
@@ -44,35 +70,33 @@ document.addEventListener("DOMContentLoaded", function() {
     examplePlane.addEventListener("click", event => {
 	  console.log("plane click");
 	});
-      });
+});
 
+AFRAME.registerComponent('target', {
+  init: function () {
+      // console.log("Init");
+      // tempTarget = document.querySelector('#example-target').object3D;
+      // myVideoEntity = document.querySelector('#example-plane');
+      // myVideoEntity.setAttribute('visible', true);
+      // myVideo = myVideoEntity.object3D;
+  },
 
-      AFRAME.registerComponent('target', {
-        init: function () {
-            // console.log("Init");
-            // tempTarget = document.querySelector('#example-target').object3D;
-            // myVideoEntity = document.querySelector('#example-plane');
-            // myVideoEntity.setAttribute('visible', true);
-            // myVideo = myVideoEntity.object3D;
-        },
+  tick: function (time, timeDelta) {
 
-        tick: function (time, timeDelta) {
+      // if(isFinde)
+      // {
+      //     let worldPosition = new THREE.Vector3();
+      //     tempTarget.getWorldPosition(worldPosition);
+      //     console.log("Target");
+      //     console.log(worldPosition);
 
-            // if(isFinde)
-            // {
-            //     let worldPosition = new THREE.Vector3();
-            //     tempTarget.getWorldPosition(worldPosition);
-            //     console.log("Target");
-            //     console.log(worldPosition);
-    
-            //     myVideo.position.copy(worldPosition);
-    
-            //     myVideoEntity.setAttribute('position', myVideo.position);
-            //     console.log("Sub");
-            //     console.log(myVideoEntity.getAttribute('position'));
-            //     // Do something on every scene tick or frame.
-            // }
+      //     myVideo.position.copy(worldPosition);
 
-          }
-      });
-     
+      //     myVideoEntity.setAttribute('position', myVideo.position);
+      //     console.log("Sub");
+      //     console.log(myVideoEntity.getAttribute('position'));
+      //     // Do something on every scene tick or frame.
+      // }
+
+    }
+});
